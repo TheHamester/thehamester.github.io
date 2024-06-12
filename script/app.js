4e365f7c51a7f5a97a5d4f8d5086ac30dc68ab7a
Ilya <75043035+TheHamester@users.noreply.github.com>
2024-06-12 20:48:28 +0300

    added dark mode

--- script/app.js
+++ script/app.js
@@ -15,3 +15,24 @@ function copyHandle() {
     navigator.clipboard.writeText("hamester");
     addNotification("Handle copied to your clipboard!");
 }
+
+function toggleDarkMode() {
+    if(localStorage.getItem("ham-bio-darkmode") == "false") {
+        document.body.classList.add("dark");
+        localStorage.setItem("ham-bio-darkmode", "true");
+    } else {
+        document.body.classList.remove("dark");
+        localStorage.setItem("ham-bio-darkmode", "false");
+    }
+}
+
+function onPageLoad() {
+    if(localStorage.getItem("ham-bio-darkmode") === null) {
+        localStorage.setItem("ham-bio-darkmode", "false");
+        return;
+    }
+
+    if(localStorage.getItem("ham-bio-darkmode") == "true") {
+        document.body.classList.add("dark");
+    }
+}
