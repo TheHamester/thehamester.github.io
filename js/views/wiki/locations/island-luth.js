49bf2bf4a8c207bb30996ca06529ef164660bf01
Ilya <75043035+TheHamester@users.noreply.github.com>
2024-06-17 23:44:28 +0300

    fixed link on island luth page

--- js/views/wiki/locations/island-luth.js
+++ js/views/wiki/locations/island-luth.js
@@ -2,6 +2,6 @@ export const title = "Island Luth";
 export const html = /* html */ `
     <p>
         Island Luth is one of the five islands orbiting the central star of the <a href="/#/wiki/locations/moryx-system">Moryx System</a>. 
-        It's the outermost island from <a href="/#/wiki/locations/moryx">. It's very icey.
+        It's the outermost island from <a href="/#/wiki/locations/moryx">Moryx</a>. It's very icey.
     </p>
 `;
