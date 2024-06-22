a727b5aa4bfa1b6eae12e72179922fed353f14c7
Ilya <75043035+TheHamester@users.noreply.github.com>
2024-06-22 17:57:19 +0300

    what?

--- ENV.js
+++ ENV.js
@@ -1,3 +1,3 @@
-const IS_DEV_ENVIRONMENT = CSSFontFeatureValuesRule;
+const IS_DEV_ENVIRONMENT = false;
 const PROD_ORIGIN = "https://thehamester.github.io/";
 const DEV_ORIGIN = "http://localhost:5500/";
